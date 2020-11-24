61fb0ae91c130c22f2cf7287fbce620c78601a6d
Bilal AMRI <iambilalamri@gmail.com>
2020-11-25 00:36:25 +0100

    [feature/Java-12]-refacto

--- src/amrib/javaversion14/JavaVersion14.java
+++ src/amrib/javaversion14/JavaVersion14.java
@@ -2,4 +2,19 @@ package amrib.javaversion14;
 
 public class JavaVersion14 {
 
+	public static void main(String[] args) {
+		String day = "W";
+//		String result = switch (day) {
+//		case "M", "W", "F" -> "MWF";
+//		case "T", "TH", "S" -> "TTS";
+//		default -> {
+//			if (day.isEmpty())
+//				yield "Please insert a valid day.";
+//			else
+//				yield "Looks like a Sunday.";
+//		}
+//
+//		};
+//		System.out.println(result);
+	}
 }
